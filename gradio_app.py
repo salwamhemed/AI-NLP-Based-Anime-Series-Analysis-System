f4d1b85096e2676e1af8b96c7e94bba4efa9b14d
Salwa <160399022+salwamhemed@users.noreply.github.com>
2024-09-28 13:17:10 +0200

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -2,9 +2,11 @@ import gradio as gr
 import pandas as pd
 from theme_classification import theme_classifier
 from characters_network import CharactersNetworkGenerator, NamedEntityRecognizer
+from character_chatbot import CharacterChatbot
+import os
 
 def get_themes(theme_list_str, subtitles_path, save_path):
-    theme_list = theme_list_str.split(',')
+    theme_list = [theme.strip() for theme in theme_list_str.split(',')]  # Ensure no extra spaces
     themeClassifier = theme_classifier(theme_list)
     output_df = themeClassifier.get_themes_result(subtitles_path, save_path)
 
@@ -14,6 +16,7 @@ def get_themes(theme_list_str, subtitles_path, save_path):
     # Ensure that only valid themes are selected
     output_df = output_df[theme_list]
 
+    # Aggregate scores and prepare output
     output_df = output_df.sum().reset_index()
     output_df.columns = ['Theme', 'Score']
 
@@ -23,15 +26,23 @@ def get_characters_network(subtitles_path, ner_path):
     characters = CharactersNetworkGenerator()
     ner = NamedEntityRecognizer()
     
+    # Get NER and character network data
     ner_df = ner.get_ners(subtitles_path, ner_path)
     characters_df = characters.generate_character_network(ner_df)
     html = characters.draw_characters_network(characters_df)
 
     return html 
+def chat_with_gon_chatbot(message, history):
+    character_chatbot = CharacterChatbot("AbdullahTarek/Naruto_Llama-3-8B_3",
+                                         huggingface_token = os.getenv('huggingface_token')
+                                         )
 
-
+    output = character_chatbot.chat(message, history)
+    output = output['content'].strip()
+    return output
 def main():
     with gr.Blocks() as demo:
+        #HXH Theme Classification section
         with gr.Row():
             with gr.Column():
                 gr.HTML("<h1>HXH Theme Classification</h1>")
@@ -39,31 +50,40 @@ def main():
                     with gr.Column():
                         plot = gr.BarPlot(x="Theme", y="Score", title="Series Themes", vertical=False, width=500, height=260)
                     with gr.Column():
-                        theme_list = gr.Textbox(label='Themes')
-                        data_path = gr.Textbox(label='Script path')
-                        save_path = gr.Textbox(label='Save path')
+                        theme_list_input = gr.Textbox(label='Themes')
+                        script_path_input = gr.Textbox(label='Script Path')
+                        save_path_input = gr.Textbox(label='Save Path')
                         get_themes_button = gr.Button("Get Themes")
 
-                        def update_plot(theme_list, data_path, save_path):
-                            output_df = get_themes(theme_list, data_path, save_path)
-                            return gr.BarPlot.update(data=output_df)
+                        def update_plot(theme_list, script_path, save_path):
+                            output_df = get_themes(theme_list, script_path, save_path)
+                            return plot.update(data=output_df)
 
-                        get_themes_button.click(update_plot, inputs=[theme_list, data_path, save_path], outputs=[plot])
+                        get_themes_button.click(update_plot, inputs=[theme_list_input, script_path_input, save_path_input], outputs=[plot])
+        #HXH Characters Network Section
         with gr.Row():
             with gr.Column():
-                gr.HTML("<h1>Characters Network </h1>")
+                gr.HTML("<h1>Characters Network</h1>")
                 with gr.Row():
                     with gr.Column():
-                        hxh_html = gr.HTML()
+                        hxh_html_output = gr.HTML()
                     with gr.Column():
-                        data_path = gr.Textbox(label='Script path')
-                        Ner_save_path = gr.Textbox(label='Ners Save Path')
+                        char_script_path_input = gr.Textbox(label='Script Path')
+                        ner_save_path_input = gr.Textbox(label='NER Save Path')
                         get_characters_network_button = gr.Button("Get Characters Network Graph")
-                        get_characters_network_button.click(get_characters_network, inputs=[data_path, Ner_save_path], outputs=[hxh_html])
 
-                        
+                        def update_characters_network(script_path, ner_save_path):
+                            html = get_characters_network(script_path, ner_save_path)
+                            return hxh_html_output.update(value=html)
 
+                        get_characters_network_button.click(update_characters_network, inputs=[char_script_path_input, ner_save_path_input], outputs=[hxh_html_output])
+        #Character Chatbot Section 
+        with gr.Row():
+            with gr.Column():
+             gr.HTML("Character Chatbot")
+             gr.ChatInterface(chat_with_gon_chatbot)
     demo.launch(share=True)
+
 
 if __name__ == '__main__':
     main()
